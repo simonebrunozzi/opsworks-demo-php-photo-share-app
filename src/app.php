0267656da3abf31e53d4881d8fd657f6b79a3ea3
Brunozzi <simoneb@14109fe117bd.ant.amazon.com>
2013-04-08 22:19:21 -0700

    Revert "Changed title"
    This reverts commit e6550af68495017d9bb6dac0d01bfd4622dd6063.

--- src/app.php
+++ src/app.php
@@ -42,7 +42,7 @@ $app->match('/', function () use ($app) {
     $images = $query->execute() ? $query->fetchAll(PDO::FETCH_ASSOC) : array();
 
     return $app['twig']->render('index.twig', array(
-        'title'  => 'My Photos (limit to 3)',
+        'title'  => 'My Photos',
         'images' => $images,
     ));
 });
